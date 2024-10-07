cc62165a6c080c9fd2ea1b565c067990ca566202
Eg0rS <Ehrgit@yandex.ru>
2024-10-07 23:56:50 +0300

    circle

--- ShapeLibrary/Shapes/Circle.cs
+++ ShapeLibrary/Shapes/Circle.cs
@@ -1,6 +1,26 @@
-﻿namespace ShapeLibrary.Shapes;
+﻿using ShapeLibrary.Shapes.Abstractions;
 
-public class Circle
+namespace ShapeLibrary.Shapes;
+
+public class Circle : IShape
 {
-    
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+            throw new ArgumentException("Радиус должен быть положительным", nameof(radius));
+
+        Radius = radius;
+    }
+
+    public Circle(Circle circle)
+    {
+        Radius = circle.Radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
 }
